dd23b6a288cda96dc4a791800109f0729c3c3f41
Irain Navas <inavasweb@gmail.com>
2025-08-26 19:53:06 -0400

    Cambios en el Header

--- src/App.js
+++ src/App.js
@@ -1,32 +1,13 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import { Container, Navbar, Nav } from 'react-bootstrap';
-
-function Home() {
-  return <h2>Bienvenido a la página de inicio</h2>;
-}
-
-function About() {
-  return <h2>Acerca de nosotros</h2>;
-}
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home"
 
 function App() {
   return (
     <Router>
-      <Navbar bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand as={Link} to="/">Food App</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link as={Link} to="/">Inicio</Nav.Link>
-            <Nav.Link as={Link} to="/about">Acerca</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-      <Container className="mt-3">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </Container>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </Router>
   );
 }
